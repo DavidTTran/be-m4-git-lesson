074b98429c11ddd1da17d8f75fcf77ca7613243a
DavidTTran <57076945+DavidTTran@users.noreply.github.com>
2020-07-01 10:22:22 -0600

    Revert "Branch1"

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -8,4 +8,18 @@ class OrderItem < ApplicationRecord
     item = Item.find(item_id)
     self.price = item.price
   end
+
+  def self.item_quantity(order_id, item_id)
+    where("order_id = ? AND item_id = ?", order_id, item_id).count
+  end
+
+  def self.item_subtotal(order_id, item_id)
+    count = item_quantity(order_id, item_id)
+    price = OrderItem.find_by(item_id: item_id).price
+    count * price
+  end
+
+  def self.total(order_id)
+    where(order_id: order_id).pluck(:price).sum
+  end
 end
