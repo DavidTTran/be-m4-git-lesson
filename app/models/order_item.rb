809a2ed93f008ca76c1265079c400c274acf99ee
David Tran <davidttemail@gmail.com>
2020-07-01 10:01:28 -0600

    Remove user subtotal

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -12,10 +12,4 @@ class OrderItem < ApplicationRecord
   def self.item_quantity(order_id, item_id)
     where("order_id = ? AND item_id = ?", order_id, item_id).count
   end
-
-  def self.item_subtotal(order_id, item_id)
-    count = item_quantity(order_id, item_id)
-    price = OrderItem.find_by(item_id: item_id).price
-    count * price
-  end
 end
